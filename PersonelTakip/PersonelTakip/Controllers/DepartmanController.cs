c70c1292967a12149b3eee2d4b63dfd060908bba
MercanYusuf <mercann.yusuff@gmail.com>
2019-11-18 14:21:33 +0300

    MVCPersonelTakip
    MVCPersonelTakip

---
+++ PersonelTakip/PersonelTakip/Controllers/DepartmanController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PersonelTakip.Models.Veritabani;
+using PagedList;
+
+namespace PersonelTakip.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        MVCPersonelEntities db = new MVCPersonelEntities();
+        // GET: Departman
+        public ActionResult Index(string ara,int sayfa=1)
+        {
+            var liste = db.TBLDepartman.Where(x=>x.Departman.Contains(ara)||ara==null).ToList().ToPagedList(sayfa,3);
+            return View(liste);
+        }
+
+        [HttpGet]
+        public ActionResult Ekle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Ekle(TBLDepartman P)
+        {
+            db.TBLDepartman.Add(P);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult SilBilgiGetir(TBLDepartman P)
+        {
+            var sil = db.TBLDepartman.Find(P.ID);
+            
+            
+            return View("SilBilgiGetir", sil);
+        }
+        public ActionResult Sil(TBLDepartman P)
+        {
+            var sil = db.TBLDepartman.Find(P.ID);
+            db.TBLDepartman.Remove(sil);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult GuncelleBilgiGetir(TBLDepartman P)
+        {
+            var guncelle = db.TBLDepartman.Find(P.ID);
+
+            return View("GuncelleBilgiGetir", guncelle);
+        }
+
+
+        public ActionResult Guncelle(TBLDepartman P)
+        {
+            var guncelle = db.TBLDepartman.Find(P.ID);
+            guncelle.Departman = P.Departman;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ PersonelTakip/PersonelTakip/Controllers/PersonelController.cs
@@ -0,0 +1,72 @@
+﻿using PersonelTakip.Models.Veritabani;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+
+namespace PersonelTakip.Controllers
+{
+    
+    public class PersonelController : Controller
+  
+    {
+        MVCPersonelEntities db = new MVCPersonelEntities();
+        // GET: Personel
+
+        public ActionResult Index(string ara,int sayfa=1)
+        {
+            return View(db.TBLPersonel.Where(x=>x.AdiSoyadi.Contains(ara)||ara==null).ToList().ToPagedList(sayfa,3));
+        }
+
+        [HttpGet]
+        public ActionResult Ekle()
+        {
+            ViewBag.DepartmanID = new SelectList(db.TBLDepartman, "ID", "Departman");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Ekle(TBLPersonel P)
+        {
+      
+            db.TBLPersonel.Add(P);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult GuncelleBilgiGetir(TBLPersonel P)
+        {
+            P = db.TBLPersonel.Find(P.ID);
+            ViewBag.DepartmanID = new SelectList(db.TBLDepartman, "ID", "Departman", P.DepartmanID);
+            return View(P);
+        }
+
+        public ActionResult Guncelle(TBLPersonel P)
+        {
+
+            db.Entry(P).State = EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SilBilgiGetir(TBLPersonel P)
+        {
+            P = db.TBLPersonel.Find(P.ID);
+
+            return View(P);
+        }
+
+        public ActionResult Sil(TBLPersonel P)
+        {
+            P = db.TBLPersonel.Find(P.ID);
+            db.Entry(P).State = EntityState.Deleted;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
